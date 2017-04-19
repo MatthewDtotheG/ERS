53b319a8488e88202e286d17d4afae8effe15974
Matthew Garrettt <mdgarrett1984@gmail.com>
2017-04-19 09:50:03 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<html>
+  <head>
+    <title> - </title>
+
+    <link href="css/APPLICATION.css" rel="stylesheet" media="screen">
+  </head>
+
+
+<body>
+
+<div id="MAIN_WRAP">
+
+
+<div class="NAV"> 
+
+	<div class="LOGO">
+ 		<a>Element Recording Studios</a>
+ 	</div>
+
+ 	<div class="NAV_ITEMS">
+ 		<a>Studio A</a>
+ 		<a>Studio B</a>
+ 		<a>Staff</a>
+ 		<a>Artists</a>
+ 		<a>Contact</a>
+ 	</div>
+
+</div>
+
+
+<div class="SLIDER">
+	IMG SLIDER
+</div>
+
+
+<div class="CONTACT_US">
+	CONTACT US
+</div>
+
+
+<div id="WRAP">
+
+  <div class="box studio0">
+  		STUDIO A
+  </div>
+
+  <div class="box studio1">
+  		STUDIO B
+  </div>
+
+</div>
+
+
+<div class="MEET">
+	MEET THE STAFF
+</div>
+
+
+<div class="SOCIAL">
+	SOCIAL
+</div>
+
+   
+
+
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.3/handlebars.runtime.min.js"></script>
+<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+<script type="text/javascript" src="js/paper-full.min.js"></script>
+<script type="text/paperscript" src="js/dot.js" canvas="myCanvas"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,86 @@
+
+//ANCHOR TAGS ===============================
+
+$(function(){
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+      var windowWidth = $(window).width();
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 2000, 'swing', function () {
+          window.location.hash = target;
+      });
+      
+      if(windowWidth < 769){
+        $('#nav-icon3').removeClass('open')
+        $('#NAVMENU').fadeOut(300)
+     }
+  });
+});
+
+
+
+//ANCHOR TAGS END ===============================
+
+
+
+
+
+
+// NAV +===========================
+
+$(document).ready(function(){  
+        $("#NAV").hide();                  
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 700) {
+                $('#NAV').fadeIn();
+            } else {
+                $('#NAV').fadeOut();
+            }
+        });
+    });
+
+// NAV  +===========================
+
+
+
+window.sr = ScrollReveal({ duration: 800 });
+sr.reveal('.box', 300);
+
+
+
+
+
+
+
+
+
+
+
+// COLLAPSIBLE NAV MENU ===============================
+
+
+$(document).ready(function(){
+  $('#nav-icon3').click(function(){
+    $(this).toggleClass('open');
+  });
+});
+
+$('#nav-icon3').click(function() {
+    $('#NAVMENU').fadeToggle(300);
+});
+
+
+
+// COLLAPSIBLE NAV MENU END ===============================
+
+
+
+
+
+
+
