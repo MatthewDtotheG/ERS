32b4a1b10afe16f2f968bf9a130816438df30bb1
Matthew Garrettt <mdgarrett1984@gmail.com>
2017-05-27 14:02:53 -0500

    Built v1 of staff page

--- staff.php
+++ staff.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title> ERS </title>
+    <title> ERS - Staff </title>
     <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
     <link href="css/APPLICATION.css" rel="stylesheet" media="screen">
   </head>
@@ -29,23 +29,165 @@
 
 </div>
 
-  <h1 class="STUFF"> Cum sociis natoque penatibus et magnis dis parturient montes,<br> nascetur ridiculus mus.
-  </h1>
 
-<div id="WRAP">
+  <h1 class="STAFF_TITLE"> Work with the best.</h1>
+  <p class="STAFF_BLURB">Our staff has decades of experience producing, recording, mixing, and mastering.</p>
 
-  <div class="box studio0">
-  		STUDIO A
+
+
+
+
+
+
+
+
+<div id="MARGIN_WRAP">
+
+  <div class="ITEMS_0">
+
+      <div class="IMG_COLOR">
+      <img src="img/JOEL.png" alt="">
+      </div>
+
+      <h2>Joel Nanos</h2>
+      <h3>Owner; Recording/Mix/Mastering Engineer</h3>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <div class="RATES">
+      <h3>Rate</h3>
+      <p>$65/hour*</p>
+      <br>
+      <h3>Single Day, 8-Hour Block Rate</h3>
+      <p>$400/Day*</p>
+    </div>
+
   </div>
 
-  <div class="box studio1">
-  		STUDIO B
+  <div class="ITEMS_0">
+
+      <img src="img/LENNON.png" alt="">
+      <h2>Lennon Bone</h2>
+      <h3>Producer; Recording & Mix Engineer</h3>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <div class="RATES">
+      <h3>Rate</h3>
+      <p>$55/hour*</p>
+      <br>
+      <h3>Single Day, 8-Hour Block Rate</h3>
+      <p>$350/Day*</p>
+    </div>
+
   </div>
 
 </div>
 
 
+<div id="MARGIN_WRAP">
 
+  <div class="ITEMS_1">
+
+      <img src="img/BRANDON.png" alt="">
+      <h2>Brandon Phillips</h2>
+      <h3>Engineer</h3>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <div class="RATES">
+      <h3>Rate</h3>
+      <p>$55/hour*</p>
+      <br>
+      <h3>Single Day, 8-Hour Block Rate</h3>
+      <p>$350/Day*</p>
+    </div>
+
+  </div>
+
+  <div class="ITEMS_1">
+
+      <img src="img/ADAM.png" alt="">
+      <h2>Adam McGill</h2>
+      <h3>Recording/Mix Engineer</h3>
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <div class="RATES">
+      <h3>Rate</h3>
+      <p>$55/hour*</p>
+      <br>
+      <h3>Single Day, 8-Hour Block Rate</h3>
+      <p>$350/Day*</p>
+    </div>
+
+  </div>
+
+  <div class="ITEMS_1">
+
+
+      <img src="img/NONAME.png" alt="">
+
+
+      <h2>Noname Jimbo</h2>
+      <h3>Senior Lackey</h3>
+     
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+    </p>
+
+    <div class="RATES">
+      <h3>Rate</h3>
+      <p>$55/hour*</p>
+      <br>
+      <h3>Single Day, 8-Hour Block Rate</h3>
+      <p>$350/Day*</p>
+    </div>
+
+  </div>
+
+</div>
+
+
+<h1 class="STAFF_BOTTOM"> *Price includes studio room fees.<br>
+A 40% Deposit is required for booking.
+</h1>
+
+<h1 class="STAFF_BOTTOM">Want to book some time or find out more?
+</h1>
+
+<div class="STAFF_CONTACT">
+  <a href="contact.php">
+    <button>Contact Us</button>
+  </a>
+</div>
 
 
 
